415b49b227b0930b2a4cdc704f9d4a9db3ccaad4
EricLi2017 <ericlisz2017@hotmail.com>
2017-08-08 22:46:52 +0800

    Optimization

--- ims/WebContent/include/js/message.js
+++ ims/WebContent/include/js/message.js
@@ -1,35 +1,41 @@
 /**
- * 
+ * Inserts a new DIV with message after the menu DIV when WebSocket received a
+ * message
  */
 $(function() {
-	var wsocket = new WebSocket("ws://localhost:8088/ims/notifyall");
+	var wsocket = new WebSocket("ws://localhost:8080/ims/notifyall");
+	wsocket.onerror = onError;
 	wsocket.onopen = onOpen;
 	wsocket.onmessage = onMessage;
 	wsocket.onclose = onClose;
-	wsocket.onerror = onError;
+	function onError(evt) {
+	}
 	function onOpen(evt) {
-		$("#msgOpen").text(evt.data);
 	}
 	function onMessage(evt) {
-		$("#msg").css({
+		// disable the previous one
+		if ($("#message").length > 0) {
+			$("#message").remove();
+		}
+
+		// enable a new one
+		var msg = $("<div></div>").text(evt.data);
+		msg.attr("id", "message");
+		msg.css({
 			"display" : "block",
 			"position" : "relative",
 			"top" : "0",
 			"left" : "0",
 			// "overflow" : "auto",
-			"border" : "2px solid lightgreen",
+			"border" : "3px solid green",
 			"border-radius" : "5px",
 			// "width" : "100%",
 			"background-color" : "#fff",
 			"color" : "red",
 			"z-index" : "-1"
 		});
-		$("#msg").text(evt.data);
-	}
+		$("#menu").after(msg);
+	}// end of function onMessage
 	function onClose(evt) {
-		$("#msgClose").text(evt.data);
-	}
-	function onError(evt) {
-		$("#msgError").text(evt.data);
 	}
 });// end of $(document).ready()
